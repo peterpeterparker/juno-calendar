11f2f20bd5abea381846a09ddd6376c2ae167143
David Dal Busco <david.dalbusco@outlook.com>
2024-08-26 15:52:32 +0200

    feat: display count from doc

--- src/lib/components/AnswersCount.svelte
+++ src/lib/components/AnswersCount.svelte
@@ -1,23 +1,11 @@
 <script lang="ts">
-	import { countDocs, type Doc } from '@junobuild/core-peer';
+	import type { Doc } from '@junobuild/core-peer';
 	import type { EventData } from '$lib/types/events';
-	import { onMount } from 'svelte';
 
 	export let eventDoc: Doc<EventData>;
 
-	let count: bigint | undefined = undefined;
-
-	onMount(async () => {
-		// TODO: catch errors
-		count = await countDocs({
-			collection: 'answers',
-			filter: {
-				matcher: {
-					description: eventDoc.key
-				}
-			}
-		});
-	});
+	let count: number | undefined = undefined;
+	$: count = eventDoc?.data.count_answers;
 </script>
 
 {#if count !== undefined}
--- src/lib/types/events.ts
+++ src/lib/types/events.ts
@@ -1,4 +1,5 @@
 export interface EventData {
 	title: string;
 	dates: number[]; // Date.now().getTime()
+	count_answers?: number;
 }
