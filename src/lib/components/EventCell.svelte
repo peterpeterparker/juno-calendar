9b704f0b4045414cac842dce59fae72df877cc4c
David Dal Busco <david.dalbusco@outlook.com>
2024-09-16 15:18:39 +0200

    feat: delete documents

--- src/lib/components/EventCell.svelte
+++ src/lib/components/EventCell.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
-	import type { Doc } from '@junobuild/core-peer';
+	import { deleteDoc, type Doc } from '@junobuild/core-peer';
 	import type { EventData } from '$lib/types/events';
 	import { formatDate } from '$lib/utils/date.utils';
 	import AnswersCount from '$lib/components/AnswersCount.svelte';
+	import { emit } from '$lib/utils/events.utils';
+	import { alertStore } from '$lib/stores/alert.store';
 
 	export let eventDoc: Doc<EventData>;
 
 	let shareUrl: string;
 	$: shareUrl = `http://localhost:5173/event/?key=${eventDoc.key}`;
+
+	const onDelete = async () => {
+		await deleteDoc({
+			collection: "events",
+			doc: eventDoc
+		});
+
+		emit({message: "exampleReload"});
+
+		alertStore.set({
+			type: 'success',
+			message: 'Document deleted!'
+		});
+	}
 </script>
 
 <tr>
@@ -22,6 +38,6 @@
 	<td class="p-2 text-left w-1/4"><AnswersCount {eventDoc} /></td>
 	<td class="p-2 text-left w-1/6"><a href={shareUrl} class="text-primary">View Link</a></td>
 	<td class="p-2 text-left w-1/6">
-		<button class="btn btn-sm btn-outline btn-error">Delete</button>
+		<button class="btn btn-sm btn-outline btn-error" on:click={onDelete}>Delete</button>
 	</td>
 </tr>
