a0c708c507afee2ffd5c4d627650721bb9178bcd
David Dal Busco <david.dalbusco@outlook.com>
2024-09-16 15:49:54 +0200

    feat: async deletion

--- src/satellite/src/lib.rs
+++ src/satellite/src/lib.rs
@@ -3,13 +3,15 @@ mod images;
 mod templates;
 mod types;
 mod assert;
+mod delete;
 
 use ic_cdk_macros::{query};
 use crate::answers::count_event_answers;
 use crate::images::generate_social_image;
-use junobuild_macros::{assert_delete_doc, on_set_doc};
-use junobuild_satellite::{AssertDeleteDocContext, include_satellite, OnSetDocContext};
+use junobuild_macros::{assert_delete_doc, on_delete_doc, on_set_doc};
+use junobuild_satellite::{AssertDeleteDocContext, include_satellite, OnDeleteDocContext, OnSetDocContext};
 use crate::assert::assert_no_events;
+use crate::delete::delete_answers;
 
 #[on_set_doc(collections = ["answers", "events"])]
 async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
@@ -23,6 +25,11 @@ async fn on_set_doc(context: OnSetDocContext) -> Result<(), String> {
 #[assert_delete_doc(collections = ["answers"])]
 fn assert_delete_doc(context: AssertDeleteDocContext) -> Result<(), String> {
     assert_no_events(&context)
+}
+
+#[on_delete_doc(collections = ["events"])]
+async fn on_delete_doc(context: OnDeleteDocContext) -> Result<(), String> {
+    delete_answers(&context)
 }
 
 #[query]
