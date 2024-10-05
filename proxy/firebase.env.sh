58cd654506eb3d61d2ffcee63327f4a5227e7fd0
David Dal Busco <david.dalbusco@outlook.com>
2024-10-05 10:36:25 +0200

    feat: pseudo secret

--- proxy/firebase.env.sh
+++ proxy/firebase.env.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 firebase functions:config:set mail.from="hey@datepicker.xyz" mail.pwd="password" mail.host="mail.infomaniak.com"
+firebase functions:config:set notifications.secret="secret"
