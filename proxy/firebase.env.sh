592de1be2980d0887e12aea6f8797099fcc548ca
David Dal Busco <david.dalbusco@outlook.com>
2024-10-05 10:42:55 +0200

    feat: pseudo secret

--- proxy/firebase.env.sh
+++ proxy/firebase.env.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 firebase functions:config:set mail.from="hey@datepicker.xyz" mail.pwd="password" mail.host="mail.infomaniak.com"
-firebase functions:config:set notifications.secret="secret"
+firebase functions:config:set notifications.token="secret"
--- proxy/functions/src/app.ts
+++ proxy/functions/src/app.ts
@@ -1,5 +1,6 @@
 import * as cors from "cors";
 import * as express from "express";
+import * as functions from "firebase-functions";
 import {sendEmail} from "./email";
 import {proxy} from "./proxy";
 
@@ -7,7 +8,31 @@ const app = express();
 app.use(express.json());
 app.use(cors({origin: true}));
 
+const token: string = functions.config().notifications.token;
+
+const assertToken = ({
+  req,
+  res,
+}: {
+  req: express.Request;
+  res: express.Response;
+}): {valid: boolean} => {
+  const authorization = req.get("authorization");
+
+  if (authorization !== `Bearer ${token}`) {
+    res.status(500).send("Access restricted.");
+    return {valid: false};
+  }
+
+  return {valid: true};
+};
+
 app.post("/notifications/email", async (req, res) => {
+  const {valid} = assertToken({req, res});
+  if (!valid) {
+    return;
+  }
+
   await proxy({req, res, fn: sendEmail});
 });
 
