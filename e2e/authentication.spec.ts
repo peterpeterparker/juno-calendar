baa7aee93a0ca26e3d554e04ade73239b31a899e
David Dal Busco <david.dalbusco@outlook.com>
2024-10-28 15:36:51 +0100

    test: wait for ii (#11)

--- e2e/authentication.spec.ts
+++ e2e/authentication.spec.ts
@@ -1,6 +1,13 @@
 import {testWithII} from '@dfinity/internet-identity-playwright';
 import {expect} from '@playwright/test';
 
+testWithII.beforeEach(async ({iiPage}) => {
+	const DOCKER_CONTAINER_URL = 'http://127.0.0.1:5987';
+	const DOCKER_INTERNET_IDENTITY_ID = 'rdmx6-jaaaa-aaaaa-aaadq-cai';
+
+	await iiPage.waitReady({url: DOCKER_CONTAINER_URL, canisterId: DOCKER_INTERNET_IDENTITY_ID});
+});
+
 const appUrl = 'http://localhost:5173';
 
 testWithII('should sign-in with a new user', async ({page, iiPage}) => {
