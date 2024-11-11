46bc8d49e5298c12934fe0f9d354b041e9f55fad
David Dal Busco <david.dalbusco@outlook.com>
2024-11-11 15:53:07 +0100

    fix: url without slash

--- src/lib/constants/app.constants.ts
+++ src/lib/constants/app.constants.ts
@@ -3,7 +3,7 @@ export const APP_NAME = 'DatePicker';
 export const PROD = import.meta.env.PROD;
 
 export const AUTH_ALTERNATIVE_ORIGINS = [
-	'https://www.datepicker.xyz/',
+	'https://www.datepicker.xyz',
 	'https://hfe4n-qaaaa-aaaal-ajxjq-cai.icp0.io'
 ];
-export const AUTH_DERIVATION_ORIGIN = 'https://datepicker.xyz/';
+export const AUTH_DERIVATION_ORIGIN = 'https://datepicker.xyz';
