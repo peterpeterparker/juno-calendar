aa48d75dba272500613b9c99d8fa5902073a2209
David Dal Busco <david.dalbusco@outlook.com>
2025-01-13 15:20:53 +0100

    feat: transform hidden

--- src/declarations/satellite/satellite.factory.did.js
+++ src/declarations/satellite/satellite.factory.did.js
@@ -3,20 +3,9 @@
 
 // @ts-expect-error
 export const idlFactory = ({ IDL }) => {
-  const HttpHeader = IDL.Record({ 'value' : IDL.Text, 'name' : IDL.Text });
-  const HttpResponse = IDL.Record({
-    'status' : IDL.Nat,
-    'body' : IDL.Vec(IDL.Nat8),
-    'headers' : IDL.Vec(HttpHeader),
-  });
-  const TransformArgs = IDL.Record({
-    'context' : IDL.Vec(IDL.Nat8),
-    'response' : HttpResponse,
-  });
   return IDL.Service({
     'build_version' : IDL.Func([], [IDL.Text], ['query']),
     'hello' : IDL.Func([IDL.Text], [IDL.Text], ['query']),
-    'transform' : IDL.Func([TransformArgs], [HttpResponse], ['query']),
   });
 };
 // @ts-expect-error
